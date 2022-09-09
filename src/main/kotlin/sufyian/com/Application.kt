08bce3201b5cbecaf387f347b413983294ef8fe9
sufyian <s.m.sufyian@gmail.com>
2022-09-09 05:11:33 +0200

    Initallized the service with dependencies

---
+++ src/main/kotlin/sufyian/com/Application.kt
@@ -0,0 +1,14 @@
+package sufyian.com
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import sufyian.com.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureMonitoring()
+        configureSerialization()
+        configureAdministration()
+        configureRouting()
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/sufyian/com/plugins/Routing.kt
@@ -0,0 +1,29 @@
+package sufyian.com.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<AuthenticationException> { call, cause ->
+            call.respond(HttpStatusCode.Unauthorized)
+        }
+        exception<AuthorizationException> { call, cause ->
+            call.respond(HttpStatusCode.Forbidden)
+        }
+
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
